1a2c499953aa904b0b8c49a601bbc78105a9b492
Tanner Barlow <tanner.barlow12@gmail.com>
2018-11-06 14:48:01 -0800

    Got policy to run locally from function

--- TaskBoardPolicies/TaskBoardPolicies/DailyPolicies.cs
+++ TaskBoardPolicies/TaskBoardPolicies/DailyPolicies.cs
@@ -13,15 +13,24 @@ namespace TaskBoardPolicies
 {
     public static class DailyPolicies
     {
+        /// <summary>
+        /// Normal Schedule: 0 0 1 * * *
+        /// 
+        ///  System.Private.CoreLib: Exception while executing function: DailyPolicies. System.Private.CoreLib: One or more errors occurred. ((Line: 1, Col: 1, Idx: 0) - (Line: 1, Col: 1, Idx: 0): Exception during deserialization). YamlDotNet: (Line: 1, Col: 1, Idx: 0) - (Line: 1, Col: 1, Idx: 0): Exception during deserialization. YamlDotNet: Property '?provider' not found on type 'TaskBoardAssistant.Models.PolicyCollection'.
+        /// </summary>
+        /// <param name="myTimer"></param>
+        /// <param name="log"></param>
+        /// <param name="context"></param>
         [FunctionName("DailyPolicies")]
         public static void Run([TimerTrigger("0 0 1 * * *")]TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             var config = GetConfig(context);
-            string storageConnectionString = config["STORAGE_CONNECTION_STRING"];
+            string connectionString = config["AzureWebJobsStorage"];
             string containerName = config["POLICY_CONTAINER_NAME"];
             string blobName = config["DAILY_POLICY_FILENAME"];
-            Assistant.ExecuteFromBlob(storageConnectionString, containerName, blobName);
+            var result = Assistant.ExecuteFromBlob(connectionString, containerName, blobName);
+            log.LogInformation($"C# Timer trigger function finished at: {DateTime.Now}");
         }
 
         private static IConfigurationRoot GetConfig(ExecutionContext context)
